7e3bff4f061c6ae83c523a3e5b3ed0445a76b649
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-03 18:12:38 +0100

    Add X social icon component.

---
+++ src/app/shared/components/socials/icons/x/x.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-x',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './x.component.html',
+  styleUrls: ['./x.component.scss']
+})
+export class XComponent {
+
+}
