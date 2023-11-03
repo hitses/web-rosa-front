80e0acf2e65c9af99e174996801dab8a77649400
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-03 18:12:14 +0100

    Add Facebook social media icon component.

---
+++ src/app/shared/components/socials/icons/facebook/facebook.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-facebook',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './facebook.component.html',
+  styleUrls: ['./facebook.component.scss']
+})
+export class FacebookComponent {
+
+}
