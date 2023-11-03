2831671d12f4fd6ca130e9303e4cae28e5d92b2e
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-03 18:11:36 +0100

    Add social media icons to socials component

---
+++ src/app/shared/components/socials/socials.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FacebookComponent } from './icons/facebook/facebook.component';
+import { InstagramComponent } from './icons/instagram/instagram.component';
+import { TiktokComponent } from './icons/tiktok/tiktok.component';
+import { XComponent } from './icons/x/x.component';
+
+@Component({
+  selector: 'app-socials',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    FacebookComponent,
+    InstagramComponent,
+    XComponent,
+    TiktokComponent,
+  ],
+  templateUrl: './socials.component.html',
+  styleUrls: ['./socials.component.scss'],
+})
+export class SocialsComponent {}
