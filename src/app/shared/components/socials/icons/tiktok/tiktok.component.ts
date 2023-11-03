c729425f737d72f6b55b964ccc181f125af945b5
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-03 18:12:30 +0100

    Add TikTok social media icon component.

---
+++ src/app/shared/components/socials/icons/tiktok/tiktok.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tiktok',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tiktok.component.html',
+  styleUrls: ['./tiktok.component.scss']
+})
+export class TiktokComponent {
+
+}
