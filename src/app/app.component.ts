619e7b5dc77e688f9f3a860a29e07f8ac5badf8c
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-03 18:10:15 +0100

    Add header component to app component HTML and import it in TypeScript file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
 })
 export class AppComponent {}
