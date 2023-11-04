666ec8b333529ae66813519c80f27bad0f5e4504
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-04 12:57:36 +0100

    Reorder header components and add navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialsComponent } from '../components/socials/socials.component';
 import { TitleComponent } from '../components/title/title.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SocialsComponent, TitleComponent],
+  imports: [CommonModule, SocialsComponent, TitleComponent, NavbarComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
