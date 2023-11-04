a7f43233e868a9f4b401e7654c7cb94689c9dd62
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-04 11:46:22 +0100

    Add title component to header and adjust socials positioning.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialsComponent } from '../components/socials/socials.component';
+import { TitleComponent } from '../components/title/title.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SocialsComponent],
+  imports: [CommonModule, SocialsComponent, TitleComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
