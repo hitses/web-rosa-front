6f192f1df7e195fe0ecf9d4ba4c73fabebe62c62
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-07 08:16:37 +0100

    Add RouterOutlet import to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
 })
 export class AppComponent {}
