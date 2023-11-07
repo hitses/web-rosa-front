e29ae5a09b8f70a5ed4007d19824caf94af1ea1f
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-07 08:16:44 +0100

    Add routes for new pages

--- src/app/app.component.routing.ts
+++ src/app/app.component.routing.ts
@@ -1,11 +1,38 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   pathMatch: 'full',
-  //   loadComponent: () => import('./app.component').then((m) => m.AppComponent),
-  // },
+  {
+    path: 'libros',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/books/books.component').then((m) => m.BooksComponent),
+  },
+  {
+    path: 'sobre-mi',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'noticias',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/news/news.component').then((m) => m.NewsComponent),
+  },
+  {
+    path: 'otras-yo',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/others/others.component').then((m) => m.OthersComponent),
+  },
+  {
+    path: 'contacto',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(
+        (m) => m.ContactComponent
+      ),
+  },
   {
     path: '**',
     redirectTo: '',
