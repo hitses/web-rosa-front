42b077b5166c2e5eea9501cb3c327ea43953e41c
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-07 07:53:54 +0100

    Update navbar component with logo and social links

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { SocialsComponent } from '../socials/socials.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, SocialsComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const logoLink = document.querySelector('.logo-link');
+    if (window.scrollY > 150) {
+      logoLink!.classList.remove('hidden-logo');
+    } else {
+      logoLink!.classList.add('hidden-logo');
+    }
+
+    const socialLinks = document.querySelector('.social-links');
+    if (window.scrollY > 30) {
+      socialLinks!.classList.remove('hidden-socials');
+    } else {
+      socialLinks!.classList.add('hidden-socials');
+    }
+  }
+}
